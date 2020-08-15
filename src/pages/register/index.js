494b9f3bd2fdc723a781a64c14c90ca383d0130e
Diego <diegossl94@gmail.com>
2020-08-15 00:16:54 -0300

    Some changes...

---
+++ src/pages/register/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+function Register () {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-sm-4">
+          <h1>Register Page</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Login from './pages/login'
+import Register from './pages/register'
 
 const routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact={true} path="/login" component={Login} />
+        <Route exact={true} path="/cadastro" component={Register} />
       </Switch>
     </BrowserRouter>
   )
